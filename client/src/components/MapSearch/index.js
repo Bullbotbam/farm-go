aaa60020e144efaaff0630dac07c417e567a15cb
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-10-05 23:35:31 -0500

    correcting google map rendering

--- client/src/components/MapSearch/index.js
+++ client/src/components/MapSearch/index.js
@@ -1,1 +1,42 @@
 import React from 'react';
+import {
+	GoogleMap,
+	useLoadScript,
+	Marker,
+	InfoWindow,
+} from '@react-google-maps/api';
+
+import mapStyles from '../../utils/mapStyles';
+
+const mapContainerStyle = {
+	width: '100vw',
+	height: '100vh',
+};
+const options = {
+	styles: mapStyles,
+};
+const center = {
+	lat: 30.267153,
+	lng: -97.743057,
+};
+
+function MapSearch() {
+	return (
+		<div>
+			<h1>
+				FarmGo Market
+				<span role="img" aria-label="leaf">
+					🌿
+				</span>
+			</h1>
+			<GoogleMap
+				mapContainerStyle={mapContainerStyle}
+				zoom={10}
+				center={center}
+				options={options}
+			></GoogleMap>
+		</div>
+	);
+}
+
+export default MapSearch;
---
+++ client/src/utils/mapStyles.js
@@ -0,0 +1,59 @@
+export default [
+	{
+		featureType: 'water',
+		elementType: 'all',
+		stylers: [
+			{
+				hue: '#1CB2BD',
+			},
+			{
+				saturation: 53,
+			},
+			{
+				lightness: -44,
+			},
+			{
+				visibility: 'on',
+			},
+		],
+	},
+	{
+		featureType: 'road',
+		elementType: 'all',
+		stylers: [
+			{
+				hue: '#1CB2BD',
+			},
+			{
+				saturation: 40,
+			},
+		],
+	},
+	{
+		featureType: 'landscape',
+		elementType: 'all',
+		stylers: [
+			{
+				hue: '#BBDC00',
+			},
+			{
+				saturation: 80,
+			},
+			{
+				lightness: -20,
+			},
+			{
+				visibility: 'on',
+			},
+		],
+	},
+	{
+		featureType: 'road.highway',
+		elementType: 'all',
+		stylers: [
+			{
+				visibility: 'on',
+			},
+		],
+	},
+];
