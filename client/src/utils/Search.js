711feae05f69f30b7e15858fc2c6ac06da5c691f
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-10-06 03:15:14 -0500

    style search box

--- client/src/utils/Search.js
+++ client/src/utils/Search.js
@@ -3,6 +3,14 @@ import usePlacesAutocomplete, {
 	getGeocode,
 	getLatLng,
 } from 'use-places-autocomplete';
+import {
+	Combobox,
+	ComboboxInput,
+	ComboboxPopover,
+	ComboboxList,
+	ComboboxOption,
+} from '@reach/combobox';
+import '@reach/combobox/styles.css';
 
 export default function ({ panTo }) {
 	// set up a perimeter of where to find searchable places
@@ -18,6 +26,40 @@ export default function ({ panTo }) {
 			radius: 10 * 1000,
 		},
 	});
+	// create a search box for the map
+	return (
+		<div>
+			<Combobox
+				onSelect={async (address) => {
+					setValue(address, false);
+					clearSuggestions;
 
-	return <div>Search</div>;
+					try {
+						const results = await getGeocode({ address });
+						const { lat, lng } = await getLatLng(results[0]);
+						panTo({ lat, lng });
+					} catch (error) {
+						console.log('error!!!');
+					}
+				}}
+			>
+				<ComboboxInput
+					value={value}
+					onChange={(e) => {
+						setValue(e.target.value);
+					}}
+					disabled={!ready}
+					placeholder="Enter an address"
+				/>
+				<ComboboxPopover>
+					<ComboboxList>
+						{status === 'OK' &&
+							data.map(({ id, description }) => {
+								<ComboboxOption key={id} value={description} />;
+							})}
+					</ComboboxList>
+				</ComboboxPopover>
+			</Combobox>
+		</div>
+	);
 }
