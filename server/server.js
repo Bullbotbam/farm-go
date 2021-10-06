07f08aa18f41a23dce792c491d43054a71339ef8
chasem <cpmcquown@gmail.com>
2021-10-06 13:07:59 -0700

    adds additional logic to server file

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+// import necessary dependencies below
 const express = require ('express');
 const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
@@ -6,4 +7,32 @@ const {typeDefs, resolvers} = require ('./schemas');
 // import auth functionality once logic for auth is implemented
 const db = require('./config/connection');
 
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    // middleware for authentication
+});
+
+server.applyMiddleWare({ app });
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// serve static assets
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')))
+};
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+});
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}`)
+        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+    })
+});
 
