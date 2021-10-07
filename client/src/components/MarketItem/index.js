6e3927aa6e580c9f7d021edad8ee24e331cb7a0a
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-10-07 15:31:58 -0500

    creating the variables for items in the market to add to the global state

--- client/src/components/MarketItem/index.js
+++ client/src/components/MarketItem/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useStoreContext } from '../../utils/GlobalState';
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 
-const Products = (item) => {
+const MarketItem = (item) => {
 	const [state, dispatch] = useStoreContext();
+
+	const { _id, image, name, quantity, price } = item;
+
+	const { cart } = state;
 };
+
+export default MarketItem;
