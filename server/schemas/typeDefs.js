ac99cdc3f479016f932f27a60678df5e00bf1f6f
chasem <cpmcquown@gmail.com>
2021-10-07 13:35:48 -0700

    adds mutations to typeDefs file

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -32,15 +32,20 @@ const typeDefs = gql`
     }
 
     type Auth {
-
+        token: ID
+        customer: Customer
     }
 
     type Query {
-
+        
     }
 
     type Mutation {
-
+        addCustomer(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+        addOrder(products: [ID]!): Order
+        updateCustomer(firstName: String, lastName: String, email: String, password: String): Customer
+        updateProduct(_id: ID!, count: Int!): Product
+        login(email: String!, password: String!): Auth
     }
 
 
