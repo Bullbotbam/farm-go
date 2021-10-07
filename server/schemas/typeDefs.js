4269869cd7e98e64f080468bee7ba17c1e76ef6f
chasem <cpmcquown@gmail.com>
2021-10-07 13:14:00 -0700

    adds typeDefs for models

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,19 +3,32 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
 
     type Customer {
-
+        _id: ID
+        firstName: String
+        lastName: String
+        email: String
+        orders: [Order]
     }
 
     type Category {
-
+        _id: ID
+        title: String
     }
 
     type Order {
-
+        _id: ID
+        purchaseDate: String
+        products: [Product]
     }
 
     type Product {
-        
+        _id: ID
+        productName: String
+        description: String
+        price: Float
+        count: Int
+        image: String
+        category: Category
     }
 
     type Auth {
@@ -29,7 +42,6 @@ const typeDefs = gql`
     type Mutation {
 
     }
-
 
 
 `;
