a8d30986832dad9dfb27b1f53319e49efd6964b6
chasem <cpmcquown@gmail.com>
2021-10-07 12:46:16 -0700

    adds necessary types to typeDefs file

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,6 +2,36 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
 
+    type Customer {
+
+    }
+
+    type Category {
+
+    }
+
+    type Order {
+
+    }
+
+    type Product {
+        
+    }
+
+    type Auth {
+
+    }
+
+    type Query {
+
+    }
+
+    type Mutation {
+
+    }
+
+
+
 `;
 
 module.exports = typeDefs;
