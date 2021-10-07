d3754753367074e31d7f1fd170bdbf08a9e59692
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-10-07 15:17:04 -0500

    using Context API Global State to act as a centralized store for the apps state

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,3 +1,23 @@
 import React, { createContext, useContext } from 'react';
 // allows components to have access to functions that handle state management
 import { useProductReducer } from './reducers';
+
+const StoreContext = createContext();
+const { Provider } = StoreContext;
+
+const StoreProvider = ({ value = [], ...props }) => {
+	const [state, dispatch] = useProductReducer({
+		product: [],
+		cart: [],
+		cartOpen: false,
+		categories: [],
+		currentCategories: '',
+	});
+	return <Provider value={[state, dispatch]} {...props} />;
+};
+
+const useStoreContext = () => {
+	return useContext(StoreContext);
+};
+
+export { StoreProvider, useStoreContext };
