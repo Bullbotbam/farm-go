a29f9dadbcf851c4d2ed388f8678e07ac8d259e6
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-10-08 01:18:55 -0500

    create product page to list all items for sale

--- client/src/components/MarketItem/index.js
+++ client/src/components/MarketItem/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useStoreContext } from '../../utils/GlobalState';
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { idbPromise, plualize } from '../../utils/helpers';
 
 const MarketItem = (item) => {
 	const [state, dispatch] = useStoreContext();
@@ -23,8 +24,24 @@ const MarketItem = (item) => {
 				type: ADD_TO_CART,
 				product: { ...item, purchasQuantity: 1 },
 			});
+			idbPromise('cart', 'put', { ...item, purchasQuantity: 1 });
 		}
 	};
+	return (
+		<div className="card px-1 py-1">
+			<Link to={`/products/${_id}`}>
+				<img src={'/images/${image}'} alt={name} />
+				<p>{name}</p>
+			</Link>
+			<div>
+				<div>
+					{quantity} {plualize('item', quantity)} in stock
+				</div>
+				<span>${price}</span>
+			</div>
+			<button onClick={addToCart}>Send to cart</button>
+		</div>
+	);
 };
 
 export default MarketItem;
