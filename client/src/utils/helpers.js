c5f8c082f3ec47582531cd381629e40ec828a041
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-10-08 00:43:39 -0500

    added indexdb helper function to take an existing web application and make it work offline

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -23,7 +23,35 @@ export function idbPromise(storeName, method, object) {
 			);
 		};
 		request.onsuccess = function (e) {
-			console.log;
+			db = request.result;
+			tx: db.transaction(storeName, 'readwrite');
+			store = tx.objectStore(storename);
+
+			db.onerror = function (e) {
+				console.log('error', e);
+			};
+
+			switch (method) {
+				case 'put':
+					store.put(object);
+					resolve(object);
+					break;
+				case 'get':
+					const all = store.getall();
+					all.onsuccess = function () {
+						resolve(all.result);
+					};
+					break;
+				case 'delete':
+					store.delete(object._id);
+					break;
+				default:
+					console.log('No valid method');
+					break;
+			}
+			tx.oncomplete = function () {
+				db.close();
+			};
 		};
 	});
 }
