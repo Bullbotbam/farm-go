50288019187aeb3b0080d875d46f490a3bd74c77
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-10-08 18:36:29 -0500

    add react router to use throughout project

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import SingUp from './components/SingUp';
 import Cart from './components/Cart';
 import MapSearch from './components/MapSearch';
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
 	return (
 		<div>
