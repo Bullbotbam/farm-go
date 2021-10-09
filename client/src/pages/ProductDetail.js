0c9fab3f5dadff224fada2f8f738fb2536847726
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-10-08 21:05:49 -0500

    create product detail page with the ability to cache data with indexdb

--- client/src/pages/ProductDetail.js
+++ client/src/pages/ProductDetail.js
@@ -12,14 +12,51 @@ import { useQuery } from '@apollo/client';
 // 	CardActions,
 // } from '@material-ui/core';
 
-const productInfo = [
-	{
-		Item: 1,
-		name: 'Apples',
-		category: categories[1]._id,
-		img: apples,
-		description: 'red delicious',
-		price: 2.99,
-		quantity: 200,
-	},
-];
+import { useStoreContext } from '../utils/GlobalState';
+import { REMOVE_FROM_CART, UPDATE_PRODUCTS } from '../utils/actions';
+import { idbPromise } from '../utils/helpers';
+
+useEffect(() => {
+	// check global state to see if in global this.state.
+	if (products.length) {
+		setCurrentProduct(products.find((product) => product._id === id));
+	}
+	// retrieve data from server
+	else if (data) {
+		dispatchEvent({
+			type: UPDATE_PRODUCTS,
+			products: data.products,
+		});
+		data.products.forEach((product) => {
+			idbPromise('products', 'put', product);
+		});
+	}
+	// get cache info from idb
+	idbPromise('products', 'get').then((indexedProducts) => {
+		dispatchEvent({
+			type: UPDATE_PRODUCTS,
+			products: indexedProducts,
+		});
+	});
+}, [products, data, loading, dispatch, id]);
+
+const addToCart = () => {
+	const itemInCart = cart.find((cartItem) => cartItem._id === id);
+	if (itemInCart) {
+		dispatch({
+			type: UPDATE_CART_QUANTITY,
+			_id: id,
+			purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+		});
+		idbPromise('cart', 'put', {
+			...itemInCart,
+			purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+		});
+	} else {
+		dispatch({
+			type: ADD_TO_CART,
+			product: { ...currentProduct, purchaseQuantity: 1 },
+		});
+		idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
+	}
+};
