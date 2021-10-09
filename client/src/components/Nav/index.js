5138a8098f0fddd01ed62c3cf517770daa3470b1
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-10-09 00:49:01 -0500

    add link to switch between pages whether logged in or not

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,6 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
 
 function Nav() {
-	function showNavigation() {}
+	function showNavigation() {
+		if (Auth.loggedIn()) {
+			return (
+				<ul className="flex-row">
+					<li className="mx-1">
+						<Link to="/orderHistory">Order History</Link>
+					</li>
+					<li className="mx-1">
+						{/* this is not using the Link component to logout or user and then refresh the application to the start */}
+						<a href="/" onClick={() => Auth.logout()}>
+							Logout
+						</a>
+					</li>
+				</ul>
+			);
+		} else {
+			return (
+				<ul className="flex-row">
+					<li className="mx-1">
+						<Link to="/signup">Signup</Link>
+					</li>
+					<li className="mx-1">
+						<Link to="/login">Login</Link>
+					</li>
+				</ul>
+			);
+		}
+	}
 }
