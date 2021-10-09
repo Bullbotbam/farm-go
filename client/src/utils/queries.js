286f88368e8845acf406eb143a381632174a4d95
RiveraDenisse <denisse_alejandrar@hotmail.com>
2021-10-09 15:37:04 -0500

    add query checkout

---
+++ client/src/utils/queries.js
@@ -0,0 +1,25 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_PRODUCTS = gql`
+  query getProducts($category: ID) {
+    products(category: $category) {
+      _id
+      productName
+      description
+      price
+      count
+      image
+      category {
+        _id
+      }
+    }
+  }
+`;
+
+export const QUERY_CHECKOUT = gql`
+  query getCheckout($products: [ID]!) {
+    checkout(products: $products) {
+      session
+    }
+  }
+`;
