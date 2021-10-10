b56cb4399735053df984bedfa760b200406a8317
Maria Mireles <mireles335@gmail.com>
2021-10-10 00:55:13 -0500

    got server running/ redid server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
-// import necessary dependencies below
-const express = require ('express');
+const express = require('express');
+
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
-const { typeDefs, resolvers } = require ('./schemas');
-
+const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
@@ -12,35 +11,43 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 const startServer = async () => {
-    
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware
-});
 
-server.applyMiddleWare({ app });
-console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
-}
+  const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers, 
+    context: authMiddleware 
+  });
+
+
+  await server.start();
+
+
+  server.applyMiddleware({ app });
+
+
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
 
 startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// serve static assets
+// Serve up static assets
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')))
-};
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
-app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
 
 db.once('open', () => {
-    app.listen(PORT, () => {
-        console.log(`API server running on port ${PORT}`)
-       
-    })
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });
+
 
