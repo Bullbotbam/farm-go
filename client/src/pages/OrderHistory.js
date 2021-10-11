4c60f2137f9c298d9ff512cd2db833d5e4e17a21
chasem <cpmcquown@gmail.com>
2021-10-10 20:28:00 -0700

    adds style via mui

--- client/src/pages/OrderHistory.js
+++ client/src/pages/OrderHistory.js
@@ -4,6 +4,13 @@ import { Link } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_CUSTOMER } from '../utils/queries';
 
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 function OrderHistory() {
 
     // const { customerData } = useQuery(QUERY_CUSTOMER);
@@ -13,31 +20,77 @@ function OrderHistory() {
     //     customer = data.customer
     // }
 
-    return (
-        <>
-          <div className="container my-1">
-            <Link to="/">
-              ← Back to Farm Go
-              </Link>
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"l
+        // image="assets/groceries/bananas.jpq"
+        alt="bananas"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Bananas
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          $4.99
+        </Typography>
+      </CardContent>
+    </Card>
     
-                <h2>Showing history for Chase</h2>
-                  <div  className="my-2">
-                    <h3>Oct. 10th, 2021</h3>
-                    <div className="flex-row">
-                        <div className="card px-1 py-1">
-                          <p>your products</p>
-                          <div>
-                            <span>$100</span>
-                          </div>
-                        </div>
-                     
-                    </div>
-                  </div>
     
-          </div>
-    
-        </>)
+  );
+}
 
-};
+
+// function OrderHistory() {
+
+//     // const { customerData } = useQuery(QUERY_CUSTOMER);
+//     // let customer;
+
+//     // if (customerData) {
+//     //     customer = data.customer
+//     // }
+
+//     return (
+//         <>
+//           <div className="container my-1">
+//             <Link to="/">
+//               ← Back to Farm Go
+//               </Link>
+    
+//                 <h2>Showing history for Chase</h2>
+//                   <div  className="my-2">
+//                     <h3>Oct. 10th, 2021</h3>
+//                     <div className="flex-row">
+//                         <div className="card px-1 py-1">
+//                           <p>your products</p>
+//                           <div>
+//                             <span>$100</span>
+//                           </div>
+//                         </div>
+                     
+//                     </div>
+//                   </div>
+//                   <div class="row">
+//     <div class="col s12 m6">
+//       <div class="card">
+//         <div class="card-image">
+//           {/* <img src="images/sample-1.jpg"> */}
+//           <span class="card-title">Card Title</span>
+//           <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+//         </div>
+//         <div class="card-content">
+//           <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+    
+//           </div>
+    
+//         </>)
+
+// };
 
 export default OrderHistory;
