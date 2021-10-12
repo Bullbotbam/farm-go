fe8e4234ad433835db1cda148db6fdfac05d5748
chasem <cpmcquown@gmail.com>
2021-10-11 23:45:29 -0700

    fixes map

--- client/src/utils/Search.js
+++ client/src/utils/Search.js
@@ -11,55 +11,8 @@ import {
 	ComboboxOption,
 } from '@reach/combobox';
 import '@reach/combobox/styles.css';
-import { getToolbarUtilityClass } from '@mui/material';
 
 export default function ({ panTo }) {
-
-	// get nearest location 
-	function getLocation(location) {
-		
-		fetch(
-			"http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + location,
-		)
-		.then(function(response) {
-			return response.json()
-		})
-		.then(function(data) {
-
-			console.log(data)
-			const ID = data.results[0].id
-
-			getLocationAddress(ID)
-		})
-		.catch(error => 
-			console.log(error)
-		);
-	};
-
-	// get nearest location's products
-	function getLocationAddress(ID) {
-		fetch(
-			"http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + ID
-		)
-		.then(function(response) {
-			return response.json()
-		})
-		.then(function(data) {
-
-			const location = data.marketdetails.Address
-
-			console.log(data.marketdetails.Address)
-
-			console.log(location) 
-
-			getTo(location);
-
-		})
-		.catch(error => 
-			console.log(error)
-		);
-	};
-
 	// set up a perimeter of where to find searchable places
 	const {
 		ready,
@@ -70,34 +23,92 @@ export default function ({ panTo }) {
 	} = usePlacesAutocomplete({
 		requestOptions: {
 			location: { lat: () => 30.267153, lng: () => -97.743057 },
-			radius: 40 * 1000,
+			radius: 10 * 1000,
 		},
 	});
+
+		// get nearest location 
+		function getLocation(location) {
+		
+			fetch(
+				"http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + location,
+			)
+			.then(function(response) {
+				return response.json()
+			})
+			.then(function(data) {
+	
+				console.log(data)
+				const ID = data.results[0].id
+	
+				getLocationAddress(ID)
+			})
+			.catch(error => 
+				console.log(error)
+			);
+		};
+	
+		// get nearest location's products
+		function getLocationAddress(ID) {
+			fetch(
+				"http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + ID
+			)
+			.then(function(response) {
+				return response.json()
+			})
+			.then(async (data) => {
+	
+				const address = data.marketdetails.Address
+				
+				
+					setValue(address, false);
+					clearSuggestions();
+					console.log(address)
+
+					try {
+						const results = await getGeocode({ address });
+						console.log(results)
+						const { lat, lng } = await getLatLng(results[0]);
+						panTo({ lat, lng });
+					} catch (error) {
+						console.log(error);
+					}
+					
+	
+			})
+			.catch(error => 
+				console.log(error)
+			);
+		};
+
+		
 	// create a search box for the map
 	return (
 		<div className="search">
 			<Combobox
-				onSelect={ async (address) => {
-					setValue(address, false);
-					clearSuggestions();
+				// onSelect={async (address) => {
+				// 	setValue(address, false);
+				// 	clearSuggestions();
+				// 	console.log(address)
 
-					try {
-						const results = await getGeocode({ address });
-						const { lat, lng } = await getLatLng(results[0]);
-						panTo({ lat, lng });
-					} catch (error) {
-						console.log('error!!!');
-					}
-				}}
+				// 	try {
+				// 		const results = await getGeocode({ address });
+				// 		console.log(results)
+				// 		const { lat, lng } = await getLatLng(results[0]);
+				// 		panTo({ lat, lng });
+				// 	} catch (error) {
+				// 		console.log('error!!!');
+				// 	}
+				// }}
 			>
 				<ComboboxInput
 					value={value}
 					onChange={(e) => {
-						// setValue(e.target.value);
+						setValue(e.target.value);
 						getLocation(e.target.value)
 					}}
 					disabled={!ready}
-					placeholder="Search by ZIP code, city or state"
+					placeholder="Enter an address"
 				/>
 				<ComboboxPopover>
 					<ComboboxList>
