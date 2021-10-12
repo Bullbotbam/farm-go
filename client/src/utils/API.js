7be1f7c71f31e06978654e7b77dcc0b22bed6240
chasem <cpmcquown@gmail.com>
2021-10-11 19:20:28 -0700

    adds fetch logic

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -7,41 +7,41 @@ export const findFarmersMarket = () => {
 	return fetch(api_url).then(response);
 };
 
-// export const searchFoodDatabase = (query) => {
-// 	return fetch(
-// 		'http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=' + id
-// 	);
-// };
+// get nearest location 
+function fetchLogic() {
 
-// function getResults(zip) {
-// 	// or
-// 	// function getResults(lat, lng) {
-// 	$.ajax({
-// 		type: 'GET',
-// 		contentType: 'application/json; charset=utf-8',
-// 		// submit a get request to the restful service zipSearch or locSearch.
-// 		url:
-// 			'http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=' +
-// 			zip,
-// 		// or
-// 		// url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/locSearch?lat=" + lat + "&lng=" + lng,
-// 		dataType: 'jsonp',
-// 		jsonpCallback: 'searchResultsHandler',
-// 	});
-// }
-// //iterate through the JSON result object.
-// function searchResultsHandler(searchResults) {
-// 	for (var key in searchresults) {
-// 		alert(key);
-// 		var results = searchresults[key];
-// 		for (var i = 0; i < results.length; i++) {
-// 			var result = results[i];
-// 			for (var key in result) {
-// 				//only do an alert on the first search result
-// 				if (i == 0) {
-// 					alert(result[key]);
-// 				}
-// 			}
-// 		}
-// 	}
-// }
+    const userLocation = document.querySelector("#zip-input").value.trim();
+    
+    fetch(
+        "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + userLocation,
+    )
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+
+        console.log(data)
+        const ID = data.results[0].id
+
+        getLocationProducts(ID)
+    })
+    .catch(error => 
+        console.log(error)
+    );
+};
+
+// get nearest location's products
+function getLocationProducts(ID) {
+    fetch(
+        "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + ID
+    )
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+        console.log(data)   
+    })
+    .catch(error => 
+        console.log(error)
+    );
+};
