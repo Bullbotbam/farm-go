3fd618a441c1a8f0800e14bee399533c3137a3a3
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-10-13 18:28:32 -0500

    Changes to homepage

--- client/src/components/ProductList/productListIndex.js
+++ client/src/components/ProductList/productListIndex.js
@@ -1,72 +1,67 @@
-import React, {useEffect} from "react";
-import { useQuery } from "@apollo/client";
-import { QUERY_PRODUCTS } from "../../utils/queries";
-import ProductItem from "../ProductItem/productItemIndex";
-import { useStoreContext } from "../../utils/GlobalState";
-import { UPDATE_PRODUCTS } from "../../utils/actions";
-import { idbPromise} from "../../utils/helpers"
-
+import React, { useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_PRODUCTS } from '../../utils/queries';
+import ProductItem from '../ProductItem/productItemIndex';
+import { useStoreContext } from '../../utils/GlobalState';
+import { UPDATE_PRODUCTS } from '../../utils/actions';
+import { idbPromise } from '../../utils/helpers';
 
 function ProductList() {
+	const [state, dispatch] = useStoreContext();
 
- const [state, dispatch] = useStoreContext();
+	const { currentCategory } = state;
 
-const { currentCategory } = state;
+	const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-const { loading, data } = useQuery(QUERY_PRODUCTS);
+	useEffect(() => {
+		if (data) {
+			dispatch({
+				type: UPDATE_PRODUCTS,
+				products: data.products,
+			});
 
-useEffect(() => {
-  if (data) {
-    dispatch({
-      type: UPDATE_PRODUCTS,
-      products: data.products
-    });
+			data.products.forEach((product) => {
+				idbPromise('products', 'put', product);
+			});
+		} else if (!loading) {
+			idbPromise('products', 'get').then((products) => {
+				dispatch({
+					type: UPDATE_PRODUCTS,
+					products: products,
+				});
+			});
+		}
+	}, [data, loading, dispatch]);
 
-    data.products.forEach((product) => {
-        idbPromise('products', 'put', product);
-    })
-  }
-  else if (!loading) {
-      idbPromise('products', 'get').then((products) => {
-          dispatch({
-              type: UPDATE_PRODUCTS,
-              products: products
-          })
-      })
-  }
-}, [data, loading, dispatch]);
+	function filterProducts() {
+		if (!currentCategory) {
+			return state.products;
+		}
 
+		return state.products.filter(
+			(product) => product.category._id === currentCategory
+		);
+	}
 
-
-function filterProducts() {
-  if (!currentCategory) {
-    return state.products;
-  }
-
-  return state.products.filter(product => product.category._id === currentCategory);
-}
-
-    return (
-        <div className="my-2">
-            {state.products.length ? (
-                <div>
-                    {filterProducts().map((product) => (
-                        <ProductItem 
-                        key={product._id}
-                        _id={product._id}
-                        image={product.image}
-                        name={product.name}
-                        price={product.price}
-                        quantity={product.quantity}
-                        />
-                    ))}
-                    </div>
-            ):(
-                <h3>hello</h3>
-            )}
-          
-        </div>
-    )
-
+	return (
+		<div className="my-2">
+			{state.products.length ? (
+				<div>
+					{filterProducts().map((product) => (
+						<ProductItem
+							key={product._id}
+							_id={product._id}
+							image={product.image}
+							name={product.name}
+							price={product.price}
+							quantity={product.quantity}
+						/>
+					))}
+				</div>
+			) : (
+				<h3>hello</h3>
+			)}
+		</div>
+	);
 }
 export default ProductList;
---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import Carousel from '../components/Carousel';
+import Jumbotron from '../components/Jumbotron';
