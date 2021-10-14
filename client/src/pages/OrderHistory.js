e1fd8672f38dfbf1025fdd86ce738b0dbfc0b42f
chasem <cpmcquown@gmail.com>
2021-10-13 18:23:25 -0700

    merging

--- client/src/pages/OrderHistory.js
+++ client/src/pages/OrderHistory.js
@@ -6,30 +6,71 @@ import { QUERY_CUSTOMER } from '../utils/queries';
 
 import bananas from '../assets/groceries/bananas.jpg'
 import coffee from '../assets/groceries/coffee.jpg'
+import bread from '../assets/groceries/bread.jpg'
 
 import Card from '@mui/material/Card';
+import { Grid } from '@mui/material';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import { isNamedType } from 'graphql';
 
 function OrderHistory() {
 
-    // const { customerData } = useQuery(QUERY_CUSTOMER);
-    // let customer;
+    const { customerData } = useQuery(QUERY_CUSTOMER);
+    let customer;
 
-    // if (customerData) {
-    //     customer = data.customer
-    // }
+    if (customerData) {
+        customer = data.customer
+    }
 
   return (
-      <>
+    <>
     <div className='container my-1'>
-        <Link to="/">
-            ← Back to Products
-        </Link>
-    <div className="my-2">  
-    <div className='flex-row'>
-    <Card  sx={{ maxWidth: 345 }}>
+    <br/>
+    <h3>Your previous purchases:</h3>
+    <br/>
+
+    {customer ? (
+      <>
+      {customer.order.products.map(({ image, name, price }, index) => (
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid item xs={2}>
+            <Card key={index} sx={{ maxWidth: 345 }}>
+              <CardMedia
+                component="img"
+                height="140"
+                image={image}
+                alt={name}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  ${price}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      ))}
+      </>
+    ) : null}
+    </div>
+
+</>
+  );
+}
+
+{/* <>
+<div className='container my-1'>
+<br/>
+<h3>Your previous purchases:</h3>
+<br/>
+<Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+  <Grid item xs={2}>
+    <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         component="img"
         height="140"
@@ -41,68 +82,78 @@ function OrderHistory() {
           Bananas
         </Typography>
         <Typography variant="body2" color="text.secondary">
+          $1.99
+        </Typography>
+      </CardContent>
+    </Card>
+  </Grid>
+  <Grid item xs={2}>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image={bread}
+        alt="bread"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Bread
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
           $2.99
         </Typography>
       </CardContent>
     </Card>
-    </div>  
-    </div>
-    </div>
-    <div className='container my-1'>
-    <div className="my-2">  
-    <div className='flex-row'>
-    <Card  sx={{ maxWidth: 345 }}>
+  </Grid>
+  <Grid item xs={2}>
+    <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         component="img"
         height="140"
-        width="140"
         image={coffee}
-        alt="bananas"
+        alt="coffee"
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           Coffee
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          $2.99
+          $6.99
         </Typography>
       </CardContent>
     </Card>
-    </div>  
-    </div>
-    </div>
-
-</>
-  );
-}
+  </Grid>
+</Grid> */}
 
 
-{/* <div>
-        <Link to="/">
-            ← Back to Products
-        </Link>
+{/* <>
+    <div className='container my-1'>
+    <br/>
+    <h3>Your previous purchases:</h3>
+    <br/>
+
     {customer ? (
-        <>
-    {customer.order.products.map(({ image, productName, price }, index) => (
-    <Card key={index} sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        height="140"l
-        src= {image}
-        alt="bananas"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {productName}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {price}
-        </Typography>
-      </CardContent>
-    </Card>
-    ))}
-    </>
-    ) : null}
-    </div> */}
+      <>
+      {customer.order.products.map(({ image, name, price }, index) => (
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid item xs={2}>
+            <Card key={index} sx={{ maxWidth: 345 }}>
+              <CardMedia
+                component="img"
+                height="140"
+                image={image}
+                alt={name}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  ${price}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid> */}
 
 export default OrderHistory;
